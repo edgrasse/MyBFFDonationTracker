0a5adc648c066f2bb1d1290f8215b9b207b28ec1
edgrasse <edgrasse@grasselegal.com>
2026-06-07 08:40:30 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — MyBFF Donation Tracker Service Worker
 // v5.5-DEV — iOS-optimized: HTML always network-first, never cached
-const CACHE_VERSION = "mybff-v5.15d-DEV-Jun05-2026";
+const CACHE_VERSION = "mybff-v5.15e-DEV-Jun05-2026";
 const CACHE_NAME = `mybff-cache-${CACHE_VERSION}`;
 
 self.addEventListener("install", event => {
