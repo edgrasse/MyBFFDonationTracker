4ec1f4c9e5475dead9703e912fca12fcddc7682d
edgrasse <edgrasse@grasselegal.com>
2026-07-02 19:19:35 -0500

    Add files via upload

---
+++ sw-dev2.js
@@ -0,0 +1,39 @@
+const CACHE_VERSION = "mybff-dev2-v6.0-Jul02-2026";
+const CACHE_NAME = `mybff-cache-${CACHE_VERSION}`;
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (event.request.method !== 'GET') return;
+  if (url.origin !== location.origin) return;
+  // Network first for HTML, cache first for everything else
+  if (event.request.destination === 'document') {
+    event.respondWith(
+      fetch(event.request).then(r => {
+        const clone = r.clone();
+        caches.open(CACHE_NAME).then(c => c.put(event.request, clone));
+        return r;
+      }).catch(() => caches.match(event.request))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(r => r || fetch(event.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(event.request, clone));
+        return res;
+      }))
+    );
+  }
+});
